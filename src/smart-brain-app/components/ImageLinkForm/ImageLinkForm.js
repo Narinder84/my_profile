b8169427b6d0640c51973c996ddf660909edec8d
getdev84 <getdev84@gmail.com>
2020-09-25 16:10:24 -0400

    only smart barain app

--- src/smart-brain-app/components/ImageLinkForm/ImageLinkForm.js
+++ src/smart-brain-app/components/ImageLinkForm/ImageLinkForm.js
@@ -1,23 +1,31 @@
+/** @format */
+
 import React from 'react';
 import './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
-  return (
-    <div>
-      <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
-      </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-          <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-pink'
-            onClick={onButtonSubmit}
-          >Detect</button>
-        </div>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div>
+			<p className='f3'>
+				{'This Magic Brain will detect faces in your pictures. Give it a try.'}
+			</p>
+			<div className='center'>
+				<div className='form center pa4 br3 shadow-5'>
+					<input
+						className='f4 pa2 w-70 center'
+						type='tex'
+						placeholder='file must end with png , exm:- abcd.png'
+						onChange={onInputChange}
+					/>
+					<button
+						className='w-30 grow f4 link ph3 pv2 dib white bg-pink'
+						onClick={onButtonSubmit}>
+						Detect
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ImageLinkForm;
